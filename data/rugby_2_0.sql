5f04af6cf880b4dd4be2609592dd5cfc32ce18bb
Victor-Voyer <victor.voyer@oclock.school>
2024-06-14 20:17:41 +0200

    update : database (stade toulousain players)

--- data/rugby_2_0.sql
+++ data/rugby_2_0.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS "teams" (
   FOREIGN KEY("championship_name") REFERENCES "championship"("name")
 );
 
-INSERT INTO "teams"("id","name","championship_name") VALUES
+INSERT INTO "teams"("id","name","championship_name",) VALUES
 (1, 'Stade Toulousain', 'Top 14'),
 (2, 'Union Bordeaux-Bègles', 'Top 14'),
 (3, 'Stade Rochelais', 'Top 14'),
 (4, 'Toulon', 'Top 14'),
 (5, 'Castres', 'Top 14'),
-(6, 'Stade Français', 'Top 14'),
+(6, 'Stade Francençais', 'Top 14' ),
 (7, 'Bayonne', 'Top 14'),
 (8, 'ASM Clermont', 'Top 14'),
 (9, 'Lyon', 'Top 14'),
@@ -86,11 +86,54 @@ CREATE TABLE IF NOT EXISTS "players" (
   "description" TEXT;
 );
 
-INSERT INTO "players"("id","first_name", "last_name", "birthday", "height", "weight", "post_id", "team_id", "championship_id", "nationality", "description") VALUES
-(1, 'David', 'Ainu''u', '20 Nov 1999', 178, 122, 1, 1, 1, 'USA', 'Arrivé des Etats-Unis en 2017, David Ainu''u a intégré le Centre de Formation du Stade Toulousain. Lors de la saison 18-19, il se retrouve propulsé en équipe première à 6 reprises, en rentrant notamment en jeu lors de la victoire des Stadistes contre le Leinster à Ernest Wallon.
+INSERT INTO "players"("id","first_name", "last_name", "birthday", "height", "weight", "post_id", "team_id", "championship_id", "nationality") VALUES
+(1, 'David', 'Ainu''u', '1999-11-20', 178, 122, 1, 1, 1, 'Etat-Unis'),
+(2, 'Dorian', 'Aldegheri', '1993-08-04', 180, 119, 1, 1, 1, 'France'),
+(3, 'Cyril', 'Baille', '1993-09-15', 182, 118, 1, 1, 1, 'France'),
+(4, 'Maxime', 'Duprat', '1999-06-12', 182, 120, 1, 1, 1, 'France'),
+(5, 'Nepo', 'Laulala', '1991-11-06', 184, 133, 1, 1, 1, 'Nouvelle-Zélande'),
+(6, 'Paul', 'Mallez', '2001-01-24', 180, 116, 1, 1, 1, 'France'),
+(7, 'Joel', 'Merkler', '2001-10-25', 194, 135, 1, 1, 1, 'Espagne'),
+(8, 'Rodrigue', 'Neti', '1995-04-26', 185, 121, 1, 1, 1, 'France'),
+(9, 'Marco', 'Trauth', '2000-09-15', 186, 110, 1, 1, 1, 'France'),
+(10, 'Ian', 'Boubila', '2002-01-17', 181, 108, 2, 1, 1, 'France'),
+(11, 'Guillaume', 'Cramont', '2000-12-29', 182, 107, 2, 1, 1, 'France'),
+(12, 'Julien', 'Marchand', '1995-05-10', 181, 110, 2, 1, 1, 'France'),
+(13, 'Peato', 'Mauvaka', '1997-01-10', 184, 112, 2, 1, 1, 'France'),
+(14, 'Richie', 'Arnold', '1990-07-01', 208, 123, 3, 1, 1, 'Australie'),
+(15, 'Joshua', 'Brennan', '2001-11-28', 199, 118, 3, 1, 1, 'France'),
+(16, 'Piula', 'Faasalele', '1988-01-22', 196, 120, 3, 1, 1, 'France'),
+(17, 'Thibaud', 'Flament', '1997-04-29', 203, 116, 3, 1, 1, 'France'),
+(18, 'Emmanuel', 'Meafou', '1998-07-12', 203, 145, 3, 1, 1, 'France'),
+(19, 'Clément', 'Vergé', '2001-09-13', 200, 123, 3, 1, 1, 'France'),
+(20, 'Léo', 'Banos', '2002-08-16', 191, 100, 4, 1, 1, 'France'),
+(21, 'François', 'Cross', '1994-03-25', 190, 111, 4, 1, 1, 'France'),
+(22, 'Anthony', 'Jelonch', '1996-07-28', 193, 108, 4, 1, 1, 'France'),
+(23, 'Alban', 'Placines', '1993-04-23', 189, 114, 4, 1, 1, 'France'),
+(24, 'Jack', 'Willis', '1996-12-24', 190, 112, 4, 1, 1, 'Angleterre'),
+(25, 'Mathis', 'Castro Ferreira', '2004-01-13', 192, 105, 5, 1, 1, 'France'),
+(26, 'Théo', 'Ntamack', '2002-05-29', 193, 110, 5, 1, 1, 'France'),
+(27, 'Alexandre', 'Roumat', '1997-06-27', 198, 110, 5, 1, 1, 'France'),
+(28, 'Antoine', 'Dupont', '1996-11-15', 174, 85, 6, 1, 1, 'France'),
+(29, 'Baptiste', 'Germain', '2000-11-21', 174, 80, 6, 1, 1, 'France'),
+(30, 'Paul', 'Graou', '1997-07-25', 177, 88, 6, 1, 1, 'France'),
+(31, 'Blair', 'Kinghorn', '1997-01-18', 193, 107, 7, 1, 1, 'Ecosse'),
+(32, 'Romain', 'Ntamack', '1999-05-01', 186, 91, 7, 1, 1, 'France'),
+(33, 'Pita', 'Ahki', '1992-09-24', 189, 101, 8, 1, 1, 'Nouvelle-Zélande'),
+(34, 'Pierre-Louis', 'Barassi', '1998-04-22', 187, 99, 8, 1, 1, 'France'),
+(35, 'Santiago', 'Chaocobares', '1999-03-31', 188, 100, 8, 1, 1, 'Argentine'),
+(36, 'Paul', 'Costes', '2003-04-04', 182, 85, 8, 1, 1, 'France'),
+(37, 'Sofiane', 'Guitoune', '1989-03-27', 184, 97, 8, 1, 1, 'France'),
+(38, 'Setareki', 'Bituniyata', '1995-08-12', 194, 112, 9, 1, 1, 'Fidji'),
+(39, 'Dimitri', 'Delibes', '1999-03-17', 190, 98, 9, 1, 1, 'France'),
+(40, 'Nelson', 'Epée', '2001-02-20', 174, 77, 9, 1, 1, 'France'),
+(41, 'Mathis', 'Lebel', '1999-03-25', 184, 90, 9, 1, 1, 'France'),
+(42, 'Arthur', 'Retière', '1997-08-01', 170, 80, 9, 1, 1, 'France'),
+(43, 'Lucas', 'Tauzin', '1998-05-21', 187, 98, 9, 1, 1, 'France'),
+(44, 'Ange', 'Capuozzo', '1999-04-30', 177, 79, 10, 1, 1, 'Italie'),
+(45, 'Juan Cruz', 'Mallia', '1996-09-11', 182, 89, 10, 1, 1, 'Argentine'),
+(46, 'Thomas', 'Ramos', '1995-07-25', 178, 87, 10, 1, 1, 'France'),
 
-En 2019, il est sélectionné par les Etats-Unis pour les Tests de novembre, pour le Rugby Championship America et pour la Coupe du Monde au Japon, dans laquelle il se blesse malheureusement dès le premier match.
 
-Lors de la saison 20-21, son temps de jeu avec l’équipe première n’a fait qu’augmenter. Joueur puissant avec une technique individuelle très développée, il a pris part à 14 matchs de Championnat et 2 matchs de Champions Cup. Le pilier polyvalent, a participé au doublé du Stade Toulousain cette année-là, gagnant la confiance des entraîneurs qui ont fait appel à lui pour entrer en cours de match ou débuter certaines rencontres de fin de saison notamment. Les deux saisons suivantes ont été du même acabit et David est pleinement installé dans la rotation au poste de pilier. Lors du dernier exercice, il participa à 22 rencontres toutes compétitions confondues et remporta le troisième Bouclier de Brennus de sa carrière.')
 
 
